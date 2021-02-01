d7050f0a0e5eaa7378135f89d3374e59974053b6
Rodrigo Moreira <rodrigomoreira07@gmail.com>
2021-01-31 22:15:04 -0300

    Reestrutura de diretório

--- ca-escola/src/main/java/br/com/mstech/escola/aplicacao/indicacao/EnviarEmailIndicacao.java
+++
@@ -1,9 +0,0 @@
-package br.com.mstech.escola.aplicacao.indicacao;
-
-import br.com.mstech.escola.dominio.indicacao.EmailIndicacao;
-
-public interface EnviarEmailIndicacao {
-    
-    void enviarEmail(EmailIndicacao email);
-
-}
--- ca-escola/src/main/java/br/com/mstech/escola/dominio/aluno/CifradorDeSenha.java
+++
@@ -1,9 +0,0 @@
-package br.com.mstech.escola.dominio.aluno;
-
-public interface CifradorDeSenha {
-    
-    String cifrarSenha(String senha);
-
-    boolean validarSenhaCifrada(String senhaCifrada, String senha);
-    
-}
